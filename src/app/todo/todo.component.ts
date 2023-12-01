3133a52ed2fc88cc7cb0fd03bb550da40d4a69c0
Ramiandra261MDG <marco.ramiandra@gmail.com>
2023-12-01 15:14:41 +0300

    Final Static

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,8 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from "@angular/cdk/drag-drop";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { ITask } from "../_model/task";
@@ -6,33 +10,34 @@ import { ITask } from "../_model/task";
 @Component({
   selector: "app-todo",
   templateUrl: "./todo.component.html",
-  styleUrls: ["./todo.component.scss"],
+  styleUrls: ["./todo.component.scss"]
 })
 export class TodoComponent implements OnInit {
-  todoForm !: FormGroup;
-  tasks: ITask [] = [];
-  inprogress: ITask [] = [];
-  done: ITask [] = [];
-  updateIndex !: any;
+  todoForm: FormGroup;
+  tasks: ITask[] = [];
+  inprogress: ITask[] = [];
+  tests: ITask[] = [];
+  done: ITask[] = [];
+  updateIndex: any;
   isEditEnabled: boolean = false;
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
     this.todoForm = this.fb.group({
-      item : ['', Validators.required ]
-    })
+      item: ["", Validators.required]
+    });
   }
+
+  ngOnInit(): void {}
 
   addTask() {
     this.tasks.push({
       description: this.todoForm.value.item,
       done: false
-    })
+    });
     this.todoForm.reset();
   }
 
-  onEdit(item: ITask, i:number) {
+  onEdit(item: ITask, i: number) {
     this.todoForm.controls["item"].setValue(item.description);
     this.updateIndex = i;
     this.isEditEnabled = true;
@@ -47,26 +52,34 @@ export class TodoComponent implements OnInit {
   }
 
   deleteTask(i: number) {
-    this.tasks.splice(i,1)
+    this.tasks.splice(i, 1);
   }
 
   deleteProgressTask(i: number) {
-    this.inprogress.splice(i,1)
+    this.inprogress.splice(i, 1);
   }
 
-  deleteDoneTask(i: number) { 
-    this.done.splice(i,1)
+  deleteTestTask(i: number) {
+    this.tests.splice(i, 1);
+  }
+
+  deleteDoneTask(i: number) {
+    this.done.splice(i, 1);
   }
 
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
   }
